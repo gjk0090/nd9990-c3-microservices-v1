d6972c886f1376cef5f9607333ee999718938b43
gjk <gaojk0090@gmail.com>
2020-04-14 20:20:31 -0400

    hard code api host for testing

--- lesson-3-Container/exercises/udacity-c3-frontend/src/environments/environment.prod.ts
+++ lesson-3-Container/exercises/udacity-c3-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  // apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ec2-3-80-152-218.compute-1.amazonaws.com:8080/api/v0'
 };
 
 /*
